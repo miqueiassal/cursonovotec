c088b6571656cd9cecc9fc12314855830edb6adb
Miqueias Salino de Almeida <110084043+miqueiassal@users.noreply.github.com>
2024-04-03 11:26:45 -0300

    Atualizar o conversor_de_base.py

--- conversor_de_base.py
+++ conversor_de_base.py
@@ -1,11 +1,11 @@
-baseinver2 = 0
+baseinver2 = 2
 base2 = 0
 base10 = int(input("escreva um número: "))
 while(base10!=0):
     baseinver2 = baseinver2*10 + base10%2
     base10 = base10//2
     print(baseinver2)
-while(baseinver2!=0):
+while(baseinver2!=2):
     base2 = base2*10 + baseinver2%10
     baseinver2 = baseinver2//10
     print(base2)
